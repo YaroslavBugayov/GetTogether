028666ec7642c86e93dc1234318f7b1a19c993d7
YaroslavBugayov <bugayov576@gmail.com>
2023-08-23 16:27:08 +0300

    gradle fixes

--- app/src/main/java/com/gettogether/presenter/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/gettogether/presenter/viewmodels/MainViewModel.kt
@@ -1,19 +1,23 @@
 package com.gettogether.presenter.viewmodels
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.gettogether.domain.models.UserBasicInfo
+import com.gettogether.domain.models.UserProfile
 import com.gettogether.domain.usecases.CreateUserUseCase
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val getUsersUseCase: CreateUserUseCase
-) {
-    private val _users = MutableLiveData<List<UserBasicInfo>>()
-    val users: MutableLiveData<List<UserBasicInfo>> = _users
+) : ViewModel() {
+    private val _users = MutableLiveData<UserProfile>()
+    val users: MutableLiveData<UserProfile> = _users
 
-    fun createUser() {
-//        viewModelScope.launch {
-//
-//        }
-//        _users.value = getUsersUseCase.execute().data!!
+
+    fun createUser(user: UserBasicInfo) {
+        viewModelScope.launch {
+            _users.value = getUsersUseCase.execute(user).data!!
+        }
     }
 }
